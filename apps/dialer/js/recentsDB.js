5b9b21f973bc9236a6555d93325570207ba257eb
Borja Salguero <borja.salguero@gmail.com>
2012-08-20 13:05:52 +0200

    First version of separate DB

--- apps/dialer/js/recentsDB.js
+++ apps/dialer/js/recentsDB.js
@@ -1,115 +1,109 @@
-alert("RecentsDBManager loaded");
+'use strict';
 
 var RecentsDBManager = {
   _dbName: 'dialerRecents',
   _dbStore: 'dialerRecents',
   _dbVersion: 1,
   _init: function rdbm_init(callback) {
-    alert("Init start");
     try {
-    	// Retrieve indexedDB Object if is available
-      var indexedDB = window.indexedDB || window.webkitIndexedDB ||
-                      window.mozIndexedDB || window.msIndexedDB;
-    	if (!indexedDB) {
-    		// TODO Do we have to implement any custom error handler?
-      	console.log('Indexed DB is not available!!!');
+    	var indexedDB = window.indexedDB || window.webkitIndexedDB ||
+                        window.mozIndexedDB || window.msIndexedDB;
+      if (!indexedDB) {
+        console.log('Indexed DB is not available!!!');
         return;
       }
-      alert("Indexed DB retrieved");
-      // Get this object in 'self'
-			var self = this;
-			// Open DB
+      var self = this;
+      // Open DB
       this.request = indexedDB.open(this._dbName, this._dbVersion);
       //Once DB is opened
-      request.onsuccess = function(event) {
+      this.request.onsuccess = function(event) {
       	//Store DB object in RecentsDBManager
         self.db = event.target.result;
-        // --------------------------------------------
-        // self.dbReady = true;
+        // If we are in browser we are going to prepopulate for testing
         if (!navigator.mozSms) {
-        	self._prepopulateDB();
+        	// We mockup the App for Dev-Team
+        	self._prepopulateDB.call(self);
+        }else {
+        	//Callback if needed
+          if (callback) {
+          	callback.call(self);
+          }
         }
-        alert("callback if needed");
-        // --------------------------------------------
-        
-        //Callback if needed
-        if(callback){
-        	callback();
-        }
-     	};
-
-      request.onerror = function(event) {
-        console.log('Database error: ' + event.target.errorCode);
+      };
+      this.request.onerror = function(event) {
+      	// TODO Do we have to implement any custom error handler?
+      	console.log('Database error: ' + event.target.errorCode);
       };
 
-      request.onupgradeneeded = function(event) {
+      this.request.onupgradeneeded = function(event) {
         var db = event.target.result;
         var objStore = db.createObjectStore('dialerRecents', { keyPath: 'date' });
         objStore.createIndex('number', 'number');
       };
-    } catch (ex) {
-    	// TODO Do we have to implement any custom error handler?
-      console.log(ex.message);
-    }
+	  }catch (ex) {
+	  	// TODO Do we have to implement any custom error handler?
+	    console.log(ex.message);
+	  }
   },
+  // Method which check if DB is ready
   _checkDB: function rdbm_checkDB(callback) {
   	var self = this;
     if (!this.db) {
       this.request.addEventListener('success', function rdbm_DBReady() {
         self.request.removeEventListener('success', rdbm_DBReady);
-        self._checkDB(callback);
+        self._checkDB.call(self, callback);
       });
       return;
     }
-    callback();
+   	if (callback && callback instanceof Function) {
+   		callback.call(this);
+   	}
   },
+  // Metod for adding an item to recents of DB
   _add: function rdbm_add(recentCall) {
-  	this._checkDB(function(){
-
-  	
-  		var txn = this.db.transaction('dialerRecents', 'readwrite');
+  	this._checkDB.call(this, function() {
+   		var txn = this.db.transaction('dialerRecents', 'readwrite');
       var store = txn.objectStore('dialerRecents');
-      var self= this;
-      var setreq = store.put(recentCall);
-      setreq.onsuccess = function sr_onsuccess() {
-        // TODO At some point with we will be able to get the app window
-        // to update the view. Relying on visibility changes until then.
-        // (and doing a full re-render)
-        self.get();
+      var self = this;
+      var request = store.put(recentCall);
+      request.onsuccess = function sr_onsuccess() {
+        self._get.call(self);
       };
-
-      setreq.onerror = function(e) {
-        console.log('dialerRecents add failure: ', e.message, setreq.errorCode);
+			request.onerror = function(e) {
+        console.log('dialerRecents add failure: ', e.message, request.errorCode);
       };
-    });
-    
+   });
   },
+  // Method for prepopulating the recents DB for Dev-team
   _prepopulateDB: function rdbm_prepopulateDB() {
-    // var recentsEntry = {
-    //   date: Date.now(),
-    //   type: 'incoming-connected',
-    //   number: '123123123'
-    // };
-    // this.add(recentsEntry);
-    
+    var recentsEntry = {
+      date: Date.now(),
+      type: 'incoming-connected',
+      number: '123123123'
+    };
+    this._add.call(this, recentsEntry);
   },
+  // Method for deleting an item from DB
   _delete: function rdbm_delete() {
-
+    //TODO Implement
   },
-  _get: function rdbm_get() {
-    // var objectStore = this.db.transaction("dialerRecents").objectStore("dialerRecents");
-    // var recents = []; 
-    // objectStore.openCursor().onsuccess = function(event) {
-    //   var cursor = event.target.result;
-    //   if (cursor) {
-    //     recents.push(cursor.value);
-    //     cursor.continue();
-    //   }
-    //   else {
-    //     alert(recents.length);
-    //     Recents.render(recents);
-    //   }
-    // };
+  // Method for retrieving all recents from DB
+  _get: function rdbm_get(callback) {
+    var objectStore = this.db.transaction('dialerRecents').objectStore('dialerRecents');
+    var recents = [];
+    objectStore.openCursor().onsuccess = function(event) {
+      var cursor = event.target.result;
+      if (cursor) {
+        recents.push(cursor.value);
+        cursor.continue();
+      }
+      else {
+        alert(recents.length);
+        if (callback instanceof Function) {
+        	callback(recents);
+        }
+      }
+    };
 
 
   }
